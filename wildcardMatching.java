eb161fa37f4de50b88d2e051df195c14bc523973
tonymiao2012 <miaozixiong@gmail.com>
2014-08-08 11:42:40 -0500

    WildCardMatching
    New algorithm

---
+++ wildcardMatching.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class wildcardMatching {
+	public static boolean isMatch(String s, String p) {
+		int i = 0;
+		int j = 0;
+		int star = -1;
+		int mark = -1;
+		while (i < s.length()) {
+			if (j < p.length()
+					&& (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+				++i;
+				++j;
+			} else if (j < p.length() && p.charAt(j) == '*') {
+				star = j++;
+				mark = i;
+			} else if (star != -1) {
+				j = star + 1;
+				i = ++mark;
+			} else {
+				return false;
+			}
+		}
+		while (j < p.length() && p.charAt(j) == '*') {
+			++j;
+		}
+		return j == p.length();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "aaab";
+		String p = "a*";
+		System.out.println(isMatch(s, p));
+	}
+
+}
