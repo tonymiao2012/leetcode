bca01aecfc116e381892c1447ac9c95e4fdf09b4
tonymiao2012 <miaozixiong@gmail.com>
2015-11-10 17:19:07 -0600

    First Missing Positive

---
+++ FirstMissingPositive.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /**    
+     * @param A: an array of integers
+     * @return: an integer
+     */
+    public int firstMissingPositive(int[] A) {
+        // write your code here   
+        if(A == null){
+            return 1;
+        }
+        
+        for(int i = 0; i < A.length; i++){
+            while(A[i] > 0 && A[i] <= A.length && A[i] != (i + 1)){
+                int tmp = A[A[i] - 1];
+                if(tmp == A[i]){
+                    break;
+                }
+                A[A[i] - 1] = A[i];
+                A[i] = tmp;
+            }
+        }
+        
+        for(int i = 0; i < A.length; i++){
+            if(A[i] != i + 1){
+                return i + 1;
+            }
+        }
+        
+        return A.length + 1;
+    }
+}
