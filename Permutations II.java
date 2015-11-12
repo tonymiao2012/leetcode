0712f69ad1c757167c2ecf724514ebd778541ec3
tonymiao2012 <miaozixiong@gmail.com>
2015-11-12 01:14:29 -0600

    Permutations II

---
+++ Permutations II.java
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of unique permutations.
+     */
+    public ArrayList<ArrayList<Integer>> permuteUnique(ArrayList<Integer> nums) {
+        // write your code here
+                ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.size() == 0){
+            return res;
+        }
+        
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+        BitSet set = new BitSet(nums.size());
+        helper(tmp, res, nums, set);
+        
+        return res;
+    }
+    
+    private void helper(ArrayList<Integer> tmp, ArrayList<ArrayList<Integer>> result, ArrayList<Integer> nums, BitSet set){
+        if(tmp != null && tmp.size() == nums.size()){
+            ArrayList<Integer> temp = new ArrayList<Integer>(tmp);
+            if(!result.contains(temp)){
+                result.add(temp);
+            }
+            tmp = new ArrayList<Integer>();
+            return;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(!set.get(i)){
+                set.set(i, true);
+                tmp.add(nums.get(i));
+                helper(tmp, result, nums, set);
+                tmp.remove(tmp.size() - 1);
+                set.flip(i);
+            }
+        }
+    }
+}
+
