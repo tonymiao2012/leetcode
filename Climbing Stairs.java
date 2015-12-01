bec95fd14cc142b0de62c081ee073944896c0b38
tonymiao2012 <miaozixiong@gmail.com>
2015-12-01 18:34:18 -0500

    Climbing Stairs

---
+++ Climbing Stairs.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    public int climbStairs(int n) {
+        // write your code here
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n - 1];
+    }
+}
