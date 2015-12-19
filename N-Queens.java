163c5f7e9cfd572162d42e3d82165b67d404fc08
tonymiao2012 <miaozixiong@gmail.com>
2015-12-19 17:22:04 -0600

    N-Queens

---
+++ N-Queens.java
@@ -0,0 +1,37 @@
+public class Solution {
+    private Set<Integer> col = new HashSet<Integer>();
+    private Set<Integer> diag1 = new HashSet<Integer>();
+    private Set<Integer> diag2 = new HashSet<Integer>();
+
+    public ArrayList<ArrayList<String>> solveNQueens(int n) {
+        ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
+        dfs(res,new ArrayList<String>(), 0, n);
+        return res;
+    }
+    private void dfs(ArrayList<ArrayList<String>> res, ArrayList<String> list, int row, int n){
+        if (row == n){
+            res.add(new ArrayList<String>(list));
+            return;
+        }
+        for (int i = 0; i < n; i++){
+            if (col.contains(i) || diag1.contains(row + i) || diag2.contains(row - i)) continue;
+
+            char[] charArray = new char[n];
+            Arrays.fill(charArray, '.');
+            charArray[i] = 'Q';
+            String rowString = new String(charArray);
+
+            list.add(rowString);
+            col.add(i);
+            diag1.add(row + i);
+            diag2.add(row - i);
+
+            dfs(res, list, row + 1, n);
+
+            list.remove(list.size() - 1);
+            col.remove(i);
+            diag1.remove(row + i);
+            diag2.remove(row - i);
+        }
+    }
+}
