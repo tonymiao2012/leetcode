368a8a653acde9066dd76cac4b2b5ae8bbfe4835
tonymiao2012 <miaozixiong@gmail.com>
2016-02-14 23:33:29 -0800

    Word Ladder

--- Word Ladder.java
+++ Word Ladder.java
@@ -1,66 +1,38 @@
-package leetcode;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
-
-public class test {
-    public static int ladderLength(String start, String end, Set<String> dict) {
-          // Use queue to help BFS
-          Queue<String> queue = new LinkedList<String>();
-          queue.add(start);
-          queue.add(null);
-
-          // Mark visited word
-          Set<String> visited = new HashSet<String>();
-          visited.add(start);
-
-          int level = 1;
-
-          while (!queue.isEmpty()) {
-            String str = queue.poll();
-
-            if (str != null) {
-              // Modify str's each character (so word distance is 1)
-              for (int i = 0; i < str.length(); i++) {
-                char[] chars = str.toCharArray();
-
-                for (char c = 'a'; c <= 'z'; c++) {
-                  chars[i] = c;
-
-                  String word = new String(chars);
-
-                  // Found the end word
-                  if (word.equals(end)) return level + 1;
-
-                  // Put it to the queue
-                  if (dict.contains(word) && !visited.contains(word)) {
-                    queue.add(word);
-                    visited.add(word);
-                  }
-                }
-              }
-            } else {
-              level++;
-
-              if (!queue.isEmpty()) { 
-                queue.add(null);
-              }
-            }
-          }
-
-          return 0;
-        }
-    
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        String begin = "qa";
-        String end = "sq";
-        Set<String> s = new HashSet<String>(Arrays.asList("si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"));
+public class Solution {
+    /**
+      * @param start, a string
+      * @param end, a string
+      * @param dict, a set of string
+      * @return an integer
+      */
+    public int ladderLength(String start, String end, Set<String> dict) {
+        // write your code here
+        Map<String, Integer> map = new HashMap<>();
+        Queue<String> q = new LinkedList<>();
+        map.put(start, 1);
+        q.offer(start);
         
-        System.out.println(ladderLength(begin,end,s));
+        while(!q.isEmpty()){
+            String word = q.poll();
+            int len = map.get(word);
+            if(word.equals(end))    
+                break;
+            for(int i = 0; i < word.length(); i++){
+                String temp = word;
+                for(char j = 'a'; j <= 'z'; j++){
+                    char[] c = temp.toCharArray();
+                    c[i] = j;
+                    String newStr = new String(c);
+                    if(!map.containsKey(newStr) && dict.contains(newStr)){
+                        map.put(newStr, len + 1);
+                        q.add(temp);
+                    }
+                }
+            }
+        }
+        if(!map.containsKey(end)){
+            return 0;
+        }
+        return map.get(end);
     }
-
 }
