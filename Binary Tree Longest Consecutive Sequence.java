393298c5711fd99fcf8474729267ba4c8716cd7a
tonymiao2012 <miaozixiong@gmail.com>
2016-04-06 14:47:13 -0700

    Binary Tree Longest Consecutive Sequence Accepted

--- Binary Tree Longest Consecutive Sequence.java
+++ Binary Tree Longest Consecutive Sequence.java
@@ -1,30 +1,44 @@
-public class solution{
-	public int longestConsecutive(TreeNode root){
-		if(root == null){
-			return 0;
-		}
-		return helper(root, 1);
-	}
-	private int helper(TreeNode root, int num){
-		if(root == null){
-			return 0;
-		}
-		if(root.left == null && root.right == null){
-			return num;
-		}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int longestConsecutive(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return helper(root, 1);
+    }
+    private int helper(TreeNode root, int num){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return num;
+        }
+ 
+        int left, right;
+        if(root.left != null && root.val + 1 == root.left.val){
+            left = helper(root.left, num + 1);
+        }else if(root.left == null){
+            left = helper(root.left, num);
+        }else{
+            left = helper(root.left, 1);    //if root.val + 1 != left.val or right.val, current sum will be set to 1.
+        }
 
-		int left, right;
-		if(root.val + 1 = root.left.val){
-			left = helper(root.left, num + 1);
-		}else{
-			left = helper(root.left, num);
-		}
-		if(root.val + 1 == root.right.val){
-			right = helper(root.right, num + 1;
-		}else{
-			right = helper(root.right, num);
-		}
+        if(root.right != null && root.val + 1 == root.right.val){
+            right = helper(root.right, num + 1);
+        }else if(root.right == null){
+            right = helper(root.right, num);
+        }else{
+            right = helper(root.right, 1);    //Same as above.
+        }
 
-		return Math.max(left, right);
-	}
+        return Math.max(Math.max(left, right), num);    //Need to compare with current root.val. See      case[2, null, 3, 2, null, 1]
+    }
 }
